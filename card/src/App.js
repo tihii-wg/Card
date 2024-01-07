94e5a391b7c38e45139d9abf94d693123eb11225
Vova <podolian@gmail.com>
2024-01-07 13:00:15 +0200

    new

--- card/src/App.js
+++ card/src/App.js
@@ -1,7 +1,16 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <img srs="../public/wolf-3022813_1280.webp" alt="avatar" />
+      <div>
+        <h2>name</h2>
+        <p>about</p>
+      </div>
+      <div>skills</div>
+    </div>
+  );
 }
 
 export default App;
